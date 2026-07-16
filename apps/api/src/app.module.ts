8a80838c0316bcff321520de439c0cc2c5c36d99
rashel-reynard <arcanexdeveloper@gmail.com>
2026-07-16 14:18:17 +0600

    feat: add GET /me/storage endpoint

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,10 +5,11 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { StorageModule } from './storage/storage.module';
 import { StorageService } from './storage.service';
+import { MeController } from './me.controller';
 
 @Module({
   imports: [PrismaModule, AuthModule, StorageModule],
-  controllers: [AppController],
+  controllers: [AppController, MeController],
   providers: [AppService, StorageService],
 })
 export class AppModule {}
--- apps/api/src/storage/storage.module.ts
+++ apps/api/src/storage/storage.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { StorageService } from '../storage.service';
+import { MeController } from '../me.controller';
 
 @Module({
+  controllers: [MeController],
   providers: [StorageService],
   exports: [StorageService],
 })
